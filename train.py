73bdfb751210e1390678b6f6979b918dc4ae4424
Prathamesh Thakur <prathamesh272000@gmail.com>
2022-05-23 01:32:35 +0530

    update training script

--- train.py
+++ train.py
@@ -14,11 +14,18 @@ from sklearn.linear_model import ElasticNet
 from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
-
+import dvc.api
 import logging
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
+
+
+path = 'wine-quality.csv'  # actual path or name of the data file
+repo = 'https://github.com/prathameshThakur/dvc-mlflow-test'
+rev = 'data-v1'  # tag/commit of the data file
+
+data_url = dvc.api.get_url(path, repo, rev)
 
 
 def eval_metrics(actual, pred):
@@ -37,11 +44,12 @@ if __name__ == "__main__":
         "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
     )
     try:
-        data = pd.read_csv(csv_url, sep=";")
+        data = pd.read_csv(data_url, sep=",")
     except Exception as e:
         logger.exception(
             "Unable to download training & test CSV, check your internet connection. Error: %s", e
         )
+
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
@@ -49,24 +57,35 @@ if __name__ == "__main__":
     # The predicted column is "quality" which is a scalar from [3, 9]
     train_x = train.drop(["quality"], axis=1)
     test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
+    train_y = train["quality"]
+    test_y = test["quality"]
+
+    # Log artifacts: columns used for model training
+    cols_x = pd.DataFrame(list(train_x.columns))
+    cols_x.to_csv('features.csv', header=False, index=False)
+    mlflow.log_artifact('features.csv')
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    with mlflow.start_run():
+    with mlflow.start_run(nested=True):
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
         predicted_qualities = lr.predict(test_x)
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        rmse, mae, r2 = eval_metrics(test_y, predicted_qualities)
 
         print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
+
+        # log data paramaeters in mlflow
+        mlflow.log_param("data_url", data_url)
+        mlflow.log_param("data_version", rev)
+        mlflow.log_param("rows", data.shape[0])
+        mlflow.log_param("columns", data.shape[1])
 
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
@@ -83,6 +102,7 @@ if __name__ == "__main__":
             # There are other ways to use the Model Registry, which depends on the use case,
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+            mlflow.sklearn.log_model(
+                lr, "model", registered_model_name="ElasticnetWineModel")
         else:
             mlflow.sklearn.log_model(lr, "model")
